4c2e9ac39ebbf4cb9e5039cf3ed3ff85bc15ddbb
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-11 22:24:20 -0700

    added a function and added api link to the function

---
+++ script.js
@@ -0,0 +1,11 @@
+// good to comment your javascript at least each function
+// Get Quote From API
+async function getQuote() {
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+
+  }
+}
+
+//On Load
+getQuote();
