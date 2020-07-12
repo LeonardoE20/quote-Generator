51f1890231950e5f5eed11048d7add9f57b64526
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-11 23:46:03 -0700

    added event listeners so that the two buttons will work once clicked

--- script.js
+++ script.js
@@ -32,5 +32,16 @@ async function getQuote() {
   }
 }
 
+//Tweet functionality: Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerHTML
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 //On Load
 getQuote();
