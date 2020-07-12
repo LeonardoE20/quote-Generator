8c3969a756d5629f6434c67484e9f4af4c734337
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-11 22:26:37 -0700

    added an error message just in case something goes wrong and cannot receive a new quote. The catch parameter is for errors.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ async function getQuote() {
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
 
+  } catch (error) {
+    console.log('whoops, no quote', error)
   }
 }
 
