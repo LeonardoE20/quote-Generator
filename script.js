f6aea8c939474b4b274cb16645328b39ff78bceb
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-11 23:17:31 -0700

    was able to get a different quote every time you load or refresh the webpage

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // good to comment your javascript at least each function
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote From API
 async function getQuote() {
-  const prxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
+    quoteText.innerText = data.quoteText;
+    authorText.innerText = data.quoteAuthor;
     console.log(data);
   } catch (error) {
     getQuote();
-    console.log('whoops, no quote', error)
+    console.log('wnoops, no quote', error);
   }
 }
 
