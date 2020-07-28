0fd8d0de995c885f5fa2a65deed349f81c32080f
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-27 20:25:53 -0700

    created my own server and replaced it and will now use my own heroku app server

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function removeLoadingSpinner() {
 // Get Quote From API
 async function getQuote() {
   showLoadingSpinner();
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const proxyUrl = 'https://quiet-reef-55875.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
